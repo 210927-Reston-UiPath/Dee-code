94ca21aa8b5cd58ccd7d9eaa75be17a1106b4401
DHscape <dyese_harvey@protonmail.com>
2021-09-30 16:06:47 -0500

    Add Friend Commit

---
+++ VB/phonebookApp/Contact.vb
@@ -0,0 +1,14 @@
+Class Contact 
+    Property Name As string
+    Property PhoneNumber As Integer
+    Public Sub New(ByVal name as string, ByVal number as Integer)
+        Me.Name = name
+        Me.PhoneNumber = number
+    End Sub
+
+
+    Overrides Function ToString() As string
+        return $"Name: {Me.Name} Number: {Me.PhoneNumber}"
+    End Function
+    
+End Class
--- VB/phonebookApp/Program.vb
+++ VB/phonebookApp/Program.vb
@@ -1,9 +1,8 @@
 Imports System
-
 Module Program
-
     Sub Main(args As String())
         Dim menu as imenu = new MainMenu()
         menu.Start()
+        Console.WriteLine("Hello world.")
     End Sub
 End Module
